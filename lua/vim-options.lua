f9f4c887afa73abf3ec66e4f24590958e4405f4d
TDC28 <89755772+TDC28@users.noreply.github.com>
2024-03-17 15:04:15 -0400

    more tweaks

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,4 +4,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.g.loaded_perl_provider = 0
 vim.g.python3_host_prog = "/Users/alexandreboutot/.nvim/.venv/bin/python"
