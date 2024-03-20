d836d6eabdd7dda1449716f0c622ed495f7fc534
TDC28 <a06x03g07t01@protonmail.com>
2024-03-20 01:07:13 -0400

    typo

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,78 +1,78 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = false,
-			},
-			hijack_cursor = true,
-			sync_root_with_cmd = true,
-			update_focused_file = {
-				enable = true,
-				update_root = false,
-			},
-			view = {
-				adaptive_size = false,
-				preserve_window_proportions = true,
-			},
-			git = {
-				enable = true,
-				ignore = true,
-			},
-			filesystem_watchers = {
-				enable = true,
-			},
-			actions = {
-				open_file = {
-					resize_window = true,
-				},
-			},
-			renderer = {
-				highlight_git = true,
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        dotfiles = false,
+      },
+      hijack_cursor = true,
+      sync_root_with_cwd = true,
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      view = {
+        adaptive_size = false,
+        preserve_window_proportions = true,
+      },
+      git = {
+        enable = true,
+        ignore = true,
+      },
+      filesystem_watchers = {
+        enable = true,
+      },
+      actions = {
+        open_file = {
+          resize_window = true,
+        },
+      },
+      renderer = {
+        highlight_git = true,
 
-				indent_markers = {
-					enable = true,
-				},
+        indent_markers = {
+          enable = true,
+        },
 
-				icons = {
-					show = {
-						file = true,
-						folder = true,
-						folder_arrow = true,
-						git = true,
-					},
+        icons = {
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = true,
+            git = true,
+          },
 
-					glyphs = {
-						default = "󰈚",
-						symlink = "",
-						folder = {
-							default = "",
-							empty = "",
-							empty_open = "",
-							open = "",
-							symlink = "",
-							symlink_open = "",
-							arrow_open = "",
-							arrow_closed = "",
-						},
-						git = {
-							unstaged = "✗",
-							staged = "✓",
-							unmerged = "",
-							renamed = "➜",
-							untracked = "★",
-							deleted = "",
-							ignored = "◌",
-						},
-					},
-				},
-			},
-		})
-	end,
+          glyphs = {
+            default = "󰈚",
+            symlink = "",
+            folder = {
+              default = "",
+              empty = "",
+              empty_open = "",
+              open = "",
+              symlink = "",
+              symlink_open = "",
+              arrow_open = "",
+              arrow_closed = "",
+            },
+            git = {
+              unstaged = "✗",
+              staged = "✓",
+              unmerged = "",
+              renamed = "➜",
+              untracked = "★",
+              deleted = "",
+              ignored = "◌",
+            },
+          },
+        },
+      },
+    })
+  end,
 }
