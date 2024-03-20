d4dc028f66d392929fb22d3acd76d5bc79048397
TDC28 <89755772+TDC28@users.noreply.github.com>
2024-03-20 01:05:31 -0400

    changes to nvimtree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,48 +1,48 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  event = "VeryLazy",
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
 
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "catppuccin",
-        component_separators = "",
-        section_separators = { left = "", right = "" },
-      },
-      sections = {
-        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-        lualine_b = { "filename", "branch" },
-        lualine_c = {
-          {
-            function()
-              local msg = "No Active Lsp"
-              local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-              local clients = vim.lsp.get_active_clients()
-              if next(clients) == nil then
-                return msg
-              end
-              for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= "null-ls" then
-                  return client.name
-                end
-              end
-              return msg
-            end,
-            icon = " LSP:",
-            color = { fg = "#ffffff" },
-          },
-        },
-        lualine_x = {},
-        lualine_y = { "filetype", "progress" },
-        lualine_z = {
-          { "location", separator = { right = "" }, left_padding = 2 },
-        },
-      },
-      inactive_sections = {
-        lualine_a = { "filename" },
-      },
-      tabline = {},
-    })
-  end,
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+				lualine_b = { "filename", "branch" },
+				lualine_c = {
+					{
+						function()
+							local msg = "No Active Lsp"
+							local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+							local clients = vim.lsp.get_active_clients()
+							if next(clients) == nil then
+								return msg
+							end
+							for _, client in ipairs(clients) do
+								local filetypes = client.config.filetypes
+								if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= "null-ls" then
+									return client.name
+								end
+							end
+							return msg
+						end,
+						icon = " LSP:",
+						color = { fg = "#ffffff" },
+					},
+				},
+				lualine_x = {},
+				lualine_y = { "filetype", "progress" },
+				lualine_z = {
+					{ "location", separator = { right = "" }, left_padding = 2 },
+				},
+			},
+			inactive_sections = {
+				lualine_a = { "filename" },
+			},
+			tabline = {},
+		})
+	end,
 }
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,19 +11,14 @@ return {
 			filters = {
 				dotfiles = false,
 			},
-			disable_netrw = true,
-			hijack_netrw = true,
 			hijack_cursor = true,
-			hijack_unnamed_buffer_when_opening = false,
-			sync_root_with_cwd = true,
+			sync_root_with_cmd = true,
 			update_focused_file = {
 				enable = true,
 				update_root = false,
 			},
 			view = {
 				adaptive_size = false,
-				side = "left",
-				width = 30,
 				preserve_window_proportions = true,
 			},
 			git = {
@@ -39,9 +34,7 @@ return {
 				},
 			},
 			renderer = {
-				root_folder_label = false,
 				highlight_git = true,
-				highlight_opened_files = "none",
 
 				indent_markers = {
 					enable = true,
