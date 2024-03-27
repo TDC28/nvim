445c54d1f398cdda69e9ea83a0f98e3fb1ec78bf
TDC28 <a06x03g07t01@protonmail.com>
2024-03-27 01:35:09 -0400

    vimtext working with skim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,8 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.cmd("set cursorline")
 vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 vim.g.loaded_perl_provider = 0
 vim.g.python3_host_prog = "/Users/alexandreboutot/.nvim/.venv/bin/python"
+vim.g.vimtex_view_method = "skim"
 vim.opt.clipboard = "unnamedplus"
