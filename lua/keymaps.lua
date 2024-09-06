6348a03d4398f10b191918921fcfeb70bd635557
TDC28 <a06x03g07t01@protonmail.com>
2024-09-06 14:29:34 -0400

    Makrdown Previewer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,8 +2,9 @@ local keymap = vim.keymap.set
 
 -- Normal mode
 keymap("n", ";", ":")
-keymap("n", "<LEADER>e", "<CMD>NvimTreeToggle<CR>")
 keymap("n", "<TAB>", "<CMD>BufferLineCycleNext<CR>")
 keymap("n", "<S-TAB>", "<CMD>BufferLineCyclePrev<CR>")
+keymap("n", "<LEADER>e", "<CMD>NvimTreeToggle<CR>")
 keymap("n", "<LEADER>ff", "<CMD>Telescope find_files<CR>")
 keymap("n", "<LEADER>fg", "<CMD>Telescope live_grep<CR>")
+keymap("n", "<LEADER>pr", "<CMD>MarkdownPreviewToggle<CR>")
