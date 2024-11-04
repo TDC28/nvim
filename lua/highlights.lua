c53f4e2f640bf29d22e06b6893240756dc832dde
TDC28 <a06x03g07t01@protonmail.com>
2024-11-03 22:55:41 -0500

    Removing nvchad

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -5,4 +5,4 @@ vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "#181B26" })
 vim.api.nvim_set_hl(0, "TelescopePromptTitle", { bg = "#181B26" })
 
 -- NvimTree
-vim.api.nvim_set_hl(0, "NvimTreeNormalFloat", { bg = "#181B26" })
+-- vim.api.nvim_set_hl(0, "NvimTreeNormalFloat", { bg = "#181B26" })
